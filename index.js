9aba5258f411af2cf8a678b6c4770e181b632be3
Shrey-Tyagi <shreytyagi18@gmail.com>
2022-09-24 15:24:57 +0530

    feat - Added username prompt

--- index.js
+++ index.js
@@ -1,64 +1,90 @@
 import readlineSync from 'readline-sync';
 import chalk from 'chalk';
 
-var quizData = [ 
-      {
-      question: "What is the capitol of Jharkhand ?",
-      answer: "Ranchi"
-    },{
-      question: "What is the capitol of Telangana ?",
-      answer: "Hyderabad"
-    },{
-      question: "What is the capitol of Mizoram ?",
-      answer: "Aizawl"
-    },{
-      question: "What is the capitol of Gujrat ?",
-      answer: "Gandhinagar"
-    },{
-      question: "What is the capitol of Assam ?",
-      answer: "Dispur"
-    },{
-      question: "What is the capitol of Andaman and Nicobar Islands ?",
-      answer: "Portblair"
-    }
-  ];
+var quizData = [
+  {
+    question: 'What is the capitol of Jharkhand ?',
+    answer: 'Ranchi',
+  },
+  {
+    question: 'What is the capitol of Telangana ?',
+    answer: 'Hyderabad',
+  },
+  {
+    question: 'What is the capitol of Mizoram ?',
+    answer: 'Aizawl',
+  },
+  {
+    question: 'What is the capitol of Gujrat ?',
+    answer: 'Gandhinagar',
+  },
+  {
+    question: 'What is the capitol of Assam ?',
+    answer: 'Dispur',
+  },
+  {
+    question: 'What is the capitol of Andaman and Nicobar Islands ?',
+    answer: 'Portblair',
+  },
+];
 
 var score = 0;
 var level = 'Noob';
-function welcome(){
-    console.log(chalk.blue("Welcome! Get ready to test your knowelege of capitals of Indian states\n\n")
-    + chalk.cyan("Each right answer gets you 3 points and each wrong answer reduces 1, you may skip the question by pressing enter")
-    + chalk.red("\n---------------------------------------------------------")
+function welcome() {
+  var userName = readlineSync.question("What's your name? ");
+
+  console.log(
+    chalk.blue(
+      'Welcome ' +
+        chalk.red(userName) +
+        '🎉 Get ready to test your knowelege of capitals of Indian states\n\n'
+    ) +
+      chalk.cyan(
+        'Each right answer gets you 3 points and each wrong answer reduces 1, you may skip the question by pressing enter'
+      ) +
+      chalk.red(
+        '\n---------------------------------------------------------'
+      )
   );
 }
 
-function game(){
-    for (var quizObj of quizData) {
+function game() {
+  for (var quizObj of quizData) {
     var answerUser = readlineSync.question(quizObj.question);
     if (answerUser === '') {
       continue;
     }
     if (answerUser.toUpperCase() === quizObj.answer.toUpperCase()) {
-      console.log(chalk.green("Good job! :)"));
-      score++;
+      console.log(chalk.green('Good job! :)'));
+      score = score + 3;
     } else {
-      console.log(chalk.red("Better luck next time :("));
+      console.log(
+        chalk.red('Better luck next time :( the right answer is ') +
+          chalk.green(quizObj.answer)
+      );
       score--;
     }
   }
 }
 
-
-function showScores(){
-      if (score > 5) {
-      level = 'Grandmaaster';
-    } else if (score > 0 && score < 5) {
-      level = 'Master';
-    }
-    console.log(chalk.red("---------------------------------------------------------"));
-    console.log(chalk.blue("\nYAY! You SCORED: ") + chalk.green(score) + chalk.blue(" and your LEVEL is ") + chalk.green(level));
+function showScores() {
+  if (score > 12) {
+    level = 'Grandmaaster';
+  } else if (score > 0 && score < 12) {
+    level = 'Master';
+  }
+  console.log(
+    chalk.red(
+      '---------------------------------------------------------'
+    )
+  );
+  console.log(
+    chalk.blue('\nYAY! You SCORED: ') +
+      chalk.green(score) +
+      chalk.blue(' and your LEVEL is ') +
+      chalk.green(level)
+  );
 }
-
 
 welcome();
 game();
